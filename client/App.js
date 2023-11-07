2c7c03c44c95249fd506e938f3712fd705d89ad2
cognaiger <21020369@vnu.edu.vn>
2023-11-07 22:47:07 +0700

    change some ui

--- client/App.js
+++ client/App.js
@@ -26,7 +26,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Opening'>
+      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name='Opening' component={Opening} options={{ headerShown: false }} />
         <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
         <Stack.Screen name='Register' component={Register} options={{ headerShown: false }} />
--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,16 +1,91 @@
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, ScrollView, Pressable, Image } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import BottomNavigator from '../components/BottomNavigator';
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+
+
 
 const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
-        <FlatList>
-            <View style={styles.bottom}>
-                <BottomNavigator />
-            </View>
-        </FlatList>
+      <ScrollView>
+        <View style={styles.topbar}>
+          <View style={{ 
+            flexDirection: 'row',
+            gap: 20
+          }}>
+            <Pressable>
+              <AntDesign name="menu-fold" size={24} color="black" />
+            </Pressable>
+            <Text style={{
+              fontSize: 14,
+              fontWeight: 700
+            }}>Home</Text>
+          </View>
+
+          <View style={{ 
+            flexDirection: 'row',
+            gap: 20
+          }}>
+            <Pressable>
+              <AntDesign name="search1" size={24} color="black" />
+            </Pressable>
+            <Pressable>
+              <Ionicons name="notifications-outline" size={24} color="black" />
+            </Pressable>
+            <Pressable>
+              <Ionicons name="cart-outline" size={24} color="black" />
+            </Pressable>
+          </View>
+        </View>
+
+        <View style={styles.adver}>
+          <Image 
+            source = {require('../assets/adver2.jpeg')}
+            resizeMode='contain'
+            style= {{
+              flex: 1,
+              width: undefined,
+              height: undefined
+            }}
+          />
+        </View>
+
+        <View styles={styles.category}>
+          <View>
+            <Pressable style={{
+              flex: 1
+            }}>
+              <Image 
+                
+              />
+              <Text>Laptop</Text>
+            </Pressable>
+          </View>
+          
+          <View>
+
+          </View>
+        </View>
+
+        <View>
+          <Text>Adver</Text>
+        </View>
+
+        <View>
+          <Text>Most popular</Text>
+        </View>
+
+        <View>
+          <Text>Top selection</Text>
+        </View>
+
+        <View tyle={styles.bottom}>
+            <Text>Hello</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
@@ -18,13 +93,29 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    bottom: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0
-    }
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  topbar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 25,
+  },
+  bottom: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  adver: {
+    flex: 1,
+    height: 120
+  },
+  category: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 30
+  }
 })
